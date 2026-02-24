0638684bef6d9a03cefa79ed8afe1414a8cdd3ae
22197 <22197@burnside.school.nz>
2026-02-25 11:09:07 +1300

    learn another way

--- routes.py
+++ routes.py
@@ -19,35 +19,48 @@ db = SQLAlchemy(app)
 class Base(DeclarativeBase):
     pass
 
-
-# model goes here using Flask-SQLAlchemy
-# farm
-class Type(Base):
-    __tablename__ = "farm"
-    farm_id : Mapped[int] = mapped_column(primary_key=True)
-    farm_name: Mapped[str] = mapped_column(String(80))
-    farms : Mapped[list["Item"]] = relationship(back_populates="type")
-
-
-
-# Pigs
-class Item(Base):
-    __tablename__ = "pigs"
-    pig_id : Mapped[int] = mapped_column(primary_key=True)
-    pig_name : Mapped[str] = mapped_column(String(80))
-    farm_id : Mapped[int] = mapped_column(ForeignKey("farm.farm_id"))
-    type : Mapped[Type] = relationship(back_populates="farms")
-
-
-# routes go here 
+ 
 @app.route('/')
 def home():
     return render_template("home.html", title="home")
 
 
+# model goes here using Flask-SQLAlchemy
+# farm
+'''class Type(Base):
+    __tablename__ = "farm"
+    farm_id : Mapped[int] = mapped_column(primary_key=True)
+    farm_name: Mapped[str] = mapped_column(String(80))
+    farms : Mapped[list["Item"]] = relationship(back_populates="type")'''
+
+
+# Pigs
+'''class Item(Base):
+    __tablename__ = "pigs"
+    pig_id : Mapped[int] = mapped_column(primary_key=True)
+    pig_name : Mapped[str] = mapped_column(String(80))
+    farm_id : Mapped[int] = mapped_column(ForeignKey("farm.farm_id"))
+    type : Mapped[Type] = relationship(back_populates="farms")'''
+
+
+class Type(db.Model):
+    __tablename__ = 'farm'
+    farm_id = db.Column(db.Integer, primary_key=True)
+    farm_name = db.Column(db.String(80))
+    farms = db.relationship('Item', back_populates='type')
+
+class Item(db.Model):
+    __tablename__ = 'pigs'
+    pig_id = db.Column(db.Integer, primary_key=True)
+    pig_name = db.Column(db.String(80))
+    farm_id = db.Column(db.Integer, db.ForeignKey('farm.farm_id'))
+    type = db.relationship('Type', back_populates='farms')
+
+
 @app.route('/alchemy')
 def alchemy():
-    pigs = db.session.execute(select(Item)).scalars()
+    '''pigs = db.session.execute(select(Item)).scalars()'''
+    pigs = Item.query.all()
     return render_template('alchemy.html', pigs=pigs)
 
 
